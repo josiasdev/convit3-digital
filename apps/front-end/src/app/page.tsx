ff2a3e959894543576bc9546c9b3818f23f21d9b
josiasdev <josiasmartins098@gmail.com>
2024-11-19 19:43:37 -0300

    feat: ajustei a tela inicial

--- apps/front-end/src/app/page.tsx
+++ apps/front-end/src/app/page.tsx
@@ -1,14 +1,24 @@
-import Pagina from "@/components/template/Pagina";
-import {Evento} from "@/core";
+import Logo from "@/components/template/Logo";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Pagina className="w-full">
-          <h1>Inicio</h1>
-        </Pagina>
+    <div className="
+    h-screen flex flex-col justify-center items-center gap-10
+    bg-[url('/background-inicio.svg')] bg-cover
+    ">
+      <div className="flex flex-col items-center gap-4">
+        <Logo/>
+        <p className="text-zinc-500 font-light w-96 leading-6 text-center select-none">
+          Crie e gerencie o convite do seu evento de forma rápida e fácil,
+          sem complicações!
+        </p>
       </div>
+      <Link href="/evento" className="botao azul text-lg uppercase">
+      Crie o seu Evento
+      </Link>
+    </div>
     </>
   );
 }
