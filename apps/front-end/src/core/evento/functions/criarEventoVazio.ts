59ca7fe84b1aff18327c92b6d9f0425073c400a4
josiasdev <josiasmartins098@gmail.com>
2024-11-18 22:18:52 -0300

    feat: criar um evento vazio

---
+++ apps/front-end/src/core/evento/functions/criarEventoVazio.ts
@@ -0,0 +1,16 @@
+import { Id } from "@/core/shared";
+import Evento from "../model/Evento";
+
+
+export default function criarEventoVazio(): Partial<Evento> {
+    return {
+        id: Id.novo(),
+        nome: "",
+        descricao: "",
+        data: new Date(),
+        local: "",
+        publicoEsperado: 100,
+        imagem: "",
+        imagemBackground: "",
+    };
+}
