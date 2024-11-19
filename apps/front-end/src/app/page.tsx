73ffcd5b18c849e0833c057ff72565069afea9df
josiasdev <josiasmartins098@gmail.com>
2024-11-19 11:21:00 -0300

    feat: usando componente pagina

--- apps/front-end/src/app/page.tsx
+++ apps/front-end/src/app/page.tsx
@@ -1,14 +1,14 @@
 import Pagina from "@/components/template/Pagina";
-
+import {Evento} from "@/core";
 
 export default function Home() {
   return (
     <>
-    <div>
-      <Pagina className="w-full">
-        <h1>Inicio</h1>
-      </Pagina>
-    </div>
+      <div>
+        <Pagina className="w-full">
+          <h1>Inicio</h1>
+        </Pagina>
+      </div>
     </>
   );
 }
