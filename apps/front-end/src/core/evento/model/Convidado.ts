363f2b139707e7c66ccb62c13ed42ca515e6b757
josiasdev <josiasmartins098@gmail.com>
2024-11-18 21:02:54 -0300

    feat: adicionando interface Convidado com variáveis para armazenar os dados

---
+++ apps/front-end/src/core/evento/model/Convidado.ts
@@ -0,0 +1,9 @@
+
+export default interface Convidado{
+    id: string;
+    nome: string;
+    email: string;
+    confirmado: boolean;
+    possuiAcompanhante: boolean;
+    qtdeAcompanhante: number;
+}
