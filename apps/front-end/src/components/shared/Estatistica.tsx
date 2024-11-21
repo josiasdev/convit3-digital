4267fb8d8ae485b9800fba1070d4939e4692d9b4
josiasdev <josiasmartins098@gmail.com>
2024-11-21 20:09:23 -0300

    feat: mostrando a estatistica do evento o texto, valor e a imagem

--- apps/front-end/src/components/shared/Estatistica.tsx
+++ apps/front-end/src/components/shared/Estatistica.tsx
@@ -8,10 +8,10 @@ export interface EstatisticaProps{
 
 export default function Estatistica(props: EstatisticaProps){
     return(
-        <div>
-            <div>
-                <span>{props.texto}</span>
-                <span>{props.valor}</span>
+        <div className="flex items-center bg-zinc-900 rounded-lg px-6 py-2.5 gap-5 ">
+            <div className="flex-1 flex flex-col ">
+                <span className="font-light text-zinc-400">{props.texto}</span>
+                <span className="text-2x1 font-black">{props.valor}</span>
             </div>
             <Image src={props.imagem} width={80} height={80} alt="icone"/>
         </div>
