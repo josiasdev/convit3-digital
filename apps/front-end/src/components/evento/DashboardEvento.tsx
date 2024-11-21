3841c00880365cff78cef491c32448b2f17609ad
josiasdev <josiasmartins098@gmail.com>
2024-11-21 18:58:33 -0300

    feat: dashboard do evento onde pegar as informações do evento e mostrar usando props

---
+++ apps/front-end/src/components/evento/DashboardEvento.tsx
@@ -0,0 +1,16 @@
+import { Evento } from "@/core"
+import InformacoesEvento from "./InformacoesEvento"
+
+export interface DashboardEventoProps{
+    evento: Evento;
+}
+
+export default function DashboardEvento(props: DashboardEventoProps){
+    return(
+        <div className="flex flex-col gap-2">
+            <div className="flex gap-2">
+                <InformacoesEvento evento={props.evento} className="flex-1"/>
+            </div>
+        </div>
+    )
+}
